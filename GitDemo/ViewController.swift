3f24d69aef422a096b12438325e9018289778eae
dhruvaldesai <dhruvaldesai1997@gmail.com>
2024-01-15 18:03:16 +0530

    sum of 2

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("hello woeld")
+        print("2+2")
     }
 
 
